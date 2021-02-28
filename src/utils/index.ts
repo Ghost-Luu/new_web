ae1bb8b2c68b73d58969832bde64251844f714e0
Ghost <1013546570@qq.com>
2021-02-28 12:30:51 +0800

    chore: 添加TS类型定义

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 // !! 将value中的值转换为布尔值
 // 将传入的对象中的value值为空的删除掉
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
@@ -15,14 +17,14 @@ export const cleanObject = (object) => {
 };
 
 // 封装mount
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
 // 封装Debounce
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [newValue, setNewValue] = useState(value);
   useEffect(() => {
     //每次在value变化以后，设置一个定时器
